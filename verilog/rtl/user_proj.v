964f877c4694332ecd8babd2ee87045cca44818a
David Lanzendörfer <leviathan@libresilicon.com>
2023-06-02 04:38:29 +0100

    Add user project

---
+++ verilog/rtl/user_proj.v
@@ -0,0 +1,95 @@
+// SPDX-FileCopyrightText: 2020 Efabless Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// SPDX-License-Identifier: Apache-2.0
+
+`default_nettype none
+/*
+ *-------------------------------------------------------------
+ *
+ * user_proj_example
+ *
+ * This is an example of a (trivially simple) user project,
+ * showing how the user project can connect to the logic
+ * analyzer, the wishbone bus, and the I/O pads.
+ *
+ * This project generates an integer count, which is output
+ * on the user area GPIO pads (digital output only).  The
+ * wishbone connection allows the project to be controlled
+ * (start and stop) from the management SoC program.
+ *
+ * See the testbenches in directory "mprj_counter" for the
+ * example programs that drive this user project.  The three
+ * testbenches are "io_ports", "la_test1", and "la_test2".
+ *
+ *-------------------------------------------------------------
+ */
+
+module user_proj (
+`ifdef USE_POWER_PINS
+    inout vccd1,	// User area 1 1.8V supply
+    inout vssd1,	// User area 1 digital ground
+`endif
+
+    // Wishbone Slave ports (WB MI A)
+    input wire wb_clk_i,
+    input wire wb_rst_i,
+    input wire wbs_stb_i,
+    input wire wbs_cyc_i,
+    input wire wbs_we_i,
+    input wire [3:0] wbs_sel_i,
+    input wire [31:0] wbs_dat_i,
+    input wire [31:0] wbs_adr_i,
+    output wire wbs_ack_o,
+    output wire [31:0] wbs_dat_o,
+
+    // Logic Analyzer Signals
+    input  wire [127:0] la_data_in,
+    output wire [127:0] la_data_out,
+    input  wire [127:0] la_oenb,
+
+    // IOs
+    input wire [15:0] io_in,
+    output wire [15:0] io_out,
+    output wire [15:0] io_oeb,
+
+    // IRQ
+    output wire [2:0] irq
+);
+    assign io_oeb = {(15){wb_rst_i}};
+
+    // IRQ
+    assign irq = 3'b000;	// Unused
+
+    // Logic analyzer maybe later
+    assign la_data_out = 128'b0;
+
+    // No I/O. It's an accelertor
+    assign io_out = 16'b0;
+
+    // Instantiate the DUT
+    AI_Accelerator_Top #(32'h3200_0000) dut (
+        .wb_clk_i(wb_clk_i),
+        .wb_rst_i(wb_rst_i),
+        .wb_cyc_i(wbs_cyc_i),
+        .wbs_sel_i(wbs_sel_i),
+        .wb_addr_i(wbs_adr_i),
+        .wb_we_i(wbs_we_i),
+        .wb_data_i(wbs_dat_i),
+        .wb_data_o(wbs_dat_o),
+        .wb_ack(wbs_ack_o),
+        .wb_stb(wbs_stb_i)
+    );
+
+endmodule
+`default_nettype wire
